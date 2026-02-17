c4b14a84a134108f70e680d14fd5708ebfad4d4b
Pranav Agale <agalepranav2015@gmail.com>
2026-02-18 01:09:10 +0530

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -1,9 +1,10 @@
 # Simple Calculator without user input
 
-a = 10
-b = 5
+a = 45
+b = 8
 
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
+
